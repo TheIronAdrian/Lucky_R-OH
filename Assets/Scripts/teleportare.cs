4ee5878bac4e1cba68a1ab30031abfd59e02ec92
andrei33333 <stefanandreiismana@gmail.com>
2023-07-05 12:41:47 +0300

    ssfdsf
    greg

--- Assets/Scripts/teleportare.cs
+++ Assets/Scripts/teleportare.cs
@@ -8,7 +8,7 @@ public class teleportare : MonoBehaviour
     // Start is called before the first frame update
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        collision.gameObject.transform.position = new Vector3(collision.gameObject.transform.position.x, collision.gameObject.transform.position.y + 13, collision.gameObject.transform.position.z);
+        collision.gameObject.transform.position = new Vector3(collision.gameObject.transform.position.x, collision.gameObject.transform.position.y + 11, collision.gameObject.transform.position.z);
         
     }
 }
